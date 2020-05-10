3521100fa6e80815f410e76d8661e80aef99fd9e
sona <sona.suresh158@gmai.com>
2020-05-10 16:22:01 +0530

    changed port

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.use('/user', UserRoute);
 const PostRoute = require('./routes/postRoute');
 app.use('/post', PostRoute);
 
+const PORT = process.env.PORT || 3000
 
-app.listen(4000, () => {
-    console.log('Server Started on PORT ' + 3000)
+app.listen(PORT, () => {
+    console.log('Server Started on PORT ' + PORT)
 })
