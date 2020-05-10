7ec030a8a80c905bbfcb6439515d841c6893bf5e
sona <sona.suresh158@gmai.com>
2020-05-10 16:16:04 +0530

    added build

---
+++ build/precache-manifest.1b0e4a6424feb5868527512bf302bcff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6462bed1592678eff20869236f1e0eb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a5a173ed6af328314c",
+    "url": "/static/css/2.46a6b073.chunk.css"
+  },
+  {
+    "revision": "6a4d73234e883d659811",
+    "url": "/static/css/main.779e2eed.chunk.css"
+  },
+  {
+    "revision": "11a5a173ed6af328314c",
+    "url": "/static/js/2.09e3ec9f.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.09e3ec9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4d73234e883d659811",
+    "url": "/static/js/main.607df00f.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
